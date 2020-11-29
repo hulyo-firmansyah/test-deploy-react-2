734fadc0d67f5e5e0c0a27d3dddc47e87b5672be
Hulyo Firmansyah <axusxzbitcomp@gmail.com>
2020-11-29 14:47:32 +0700

    Updates

---
+++ precache-manifest.e7daf3f33b5a3cb93ae21ec90cb33153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e3a0631e7b1ed9cebcc7e2ef187451",
+    "url": "/test-deploy-react-2/index.html"
+  },
+  {
+    "revision": "5a209cc9260f837d1535",
+    "url": "/test-deploy-react-2/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "956e9a75ce63dd956d77",
+    "url": "/test-deploy-react-2/static/js/2.8b2bc2ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-deploy-react-2/static/js/2.8b2bc2ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a209cc9260f837d1535",
+    "url": "/test-deploy-react-2/static/js/main.37e1ddd7.chunk.js"
+  },
+  {
+    "revision": "15c33449f9d9a7be74ac",
+    "url": "/test-deploy-react-2/static/js/runtime-main.3ffa1d6f.js"
+  }
+]);
